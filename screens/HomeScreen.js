d7ba7be00b34db46b7a0903b14aa2c090d025424
Wsaeauiwawa <wathinee477@gmail.com>
2021-08-03 00:43:43 +0700

    updated UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,57 +1,66 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Image, Text, View, TouchableOpacity, Button, Flatlist, ScrollView, SafeAreaView } from 'react-native';
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 import Tflite from 'tflite-react-native';
 import ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 let tflite = new Tflite();
 
 const height = 350;
 const width = 350;
-const blue = "#25d5fd";
-const mobile = "Classify";
+const blue = '#00BCD4';
+const mobile = 'Predict';
 
 export default class HomeScreen extends Component<Props> {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       model: null,
       source: null,
       imageHeight: height,
       imageWidth: width,
-      recognitions: []
-
+      recognitions: [],
     };
   }
 
   onSelectModel(model) {
-    this.setState({ model });
+    this.setState({model});
     if (model == mobile) {
-        var modelFile = 'models/model_fp32.tflite';
-        var labelsFile = 'models/model.txt';
+      var modelFile = 'models/model_fp32.tflite';
+      var labelsFile = 'models/model.txt';
     }
 
-    tflite.loadModel({
-      model: modelFile,
-      labels: labelsFile,
-    },
+    tflite.loadModel(
+      {
+        model: modelFile,
+        labels: labelsFile,
+      },
       (err, res) => {
-        if (err)
-          console.log(err);
-        else
-          console.log(res);
-      }
+        if (err) console.log(err);
+        else console.log(res);
+      },
     );
   }
 
   onSelectImage() {
     const options = {
-       title: 'Select Image',
+      title: 'Select Image',
       storageOptions: {
         skipBackup: true,
         path: 'images',
       },
     };
-     ImagePicker.showImagePicker(options, (response) => {
+    ImagePicker.showImagePicker(options, response => {
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -59,148 +68,194 @@ export default class HomeScreen extends Component<Props> {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        var path = Platform.OS === 'ios' ? response.uri : 'file://' + response.path;
+        var path =
+          Platform.OS === 'ios' ? response.uri : 'file://' + response.path;
         // var w = response.width;
         // var h = response.height;
         this.setState({
-          source: { uri: path },
+          source: {uri: path},
           // imageHeight: h * width / w,
           // imageWidth: width
           imageHeight: 350,
-          imageWidth: 350
+          imageWidth: 350,
         });
         if (this.state.model) {
-            tflite.runModelOnImage({
+          tflite.runModelOnImage(
+            {
               path,
               imageMean: 128.0,
               imageStd: 128.0,
               numResults: 3,
-              threshold: 0.05
+              threshold: 0.05,
             },
-              (err, res) => {
-                if (err)
-                  console.log(err);
-                else
-                  this.setState({ recognitions: res });
-              });
+            (err, res) => {
+              if (err) console.log(err);
+              else this.setState({recognitions: res});
+            },
+          );
         }
-
       }
     });
   }
 
-   renderResults() {
-    const { model, recognitions, imageHeight, imageWidth } = this.state;
+  renderResults() {
+    const {model, recognitions, imageHeight, imageWidth} = this.state;
     if (model == mobile) {
-        return recognitions.map((res, id) => {
-          // console.log(id)
-          console.log(res.index)
-          return (
-            <TouchableOpacity onPress={() => { this.props.navigation.navigate("Predict",{index:res.index}) }} >
-            <Text key={res["index"]} style={{ color: '#000' }}>
-              {res["index"] + res["label"] + "-" + (res["confidence"] * 100).toFixed(0) + "%"}
+      return recognitions.map((res, id) => {
+        // console.log(id)
+        console.log(res.index);
+        return (
+          <TouchableOpacity
+            style={{
+              backgroundColor: blue,
+              borderRadius: 10,
+              height: 60,
+              width: 250,
+              marginTop: 10,
+              marginLeft: 50,
+              marginBottom: 10,
+            }}
+            onPress={() => {
+              this.props.navigation.navigate('Predict', {index: res.index});
+            }}>
+            <Text key={res['index']} style={{color: '#000', paddingLeft: 10, marginTop: 15}}>
+              {res['label'] + ' -' + (res['confidence'] * 100).toFixed(0) + '%'}
             </Text>
-            </TouchableOpacity>
-          )
-        });
+            <Icon
+              name="arrow-right"
+              size={20}
+              color="#D3D3D3"
+              style={{paddingLeft: 220}}
+            />
+          </TouchableOpacity>
+        );
+      });
     }
   }
 
   render() {
-   const { model, source, imageHeight, imageWidth } = this.state;
-    var renderButton = (m) => {
+    const {model, source, imageHeight, imageWidth} = this.state;
+    var renderButton = m => {
       return (
-        <TouchableOpacity style={styles.button} onPress={this.onSelectModel.bind(this, m)}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onSelectModel.bind(this, m)}>
+          <Icon
+            name="camera"
+            size={50}
+            color="#fff"
+            style={{marginVertical: 10}}
+          />
           <Text style={styles.buttonText}>{m}</Text>
         </TouchableOpacity>
-         
       );
-    }
-    return(
-      <SafeAreaView style={{flex:1}}>
-      <View style={{flex:1,flexDirection:'column',justifyContent:'flex-start',alignItems:'center'}}>
-      <ScrollView scrollEnable={true} showsVerticalScrollIndictor={true}>
-      {/* <TouchableOpacity style={styles.button} onPress={this.onSelectImage.bind(this)}>
-          <Text style={styles.buttonText}>Predict</Text>
-      </TouchableOpacity> */}
-      
-      {/* <Button
-        title="second"
-        onPress={() => this.props.navigation.navigate("Predict")}
-      /> */}
-      {model ?  
-          <TouchableOpacity style={
-            [styles.imageContainer, {
-              height: imageHeight,
-              width: imageWidth,
-              borderWidth: source ? 0 : 2
-            }]} onPress={this.onSelectImage.bind(this)}>
-            {
-              source ?
-                <Image source={source} style={{
-                  height: imageHeight, width: imageWidth
-                }} resizeMode="contain" /> :
-                <Text style={styles.text}>Select Picture</Text>
-            }
-            {/* <View>
-              {this.renderResults()}
-            </View> */}
-          </TouchableOpacity>
-
-          :
-
-          <View>
-            {renderButton(mobile)}
-          </View>
-      
-          
-          
-          
-        }
-        <View>
-            {this.renderResults()}
+    };
+    return (
+      <View style={{flex: 1, backgroundColor: '#eff2f5'}}>
+        <View style={{height: '35%'}}>
+          <Image
+            source={require('../assets/background.png')}
+            resizeMode="cover"
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+          />
         </View>
-        {/* <View>
-            {this.renderResults()}
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            backgroundColor: '#eff2f5',
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+            marginTop: -40,
+          }}>
+          <ScrollView scrollEnable={true} showsVerticalScrollIndictor={true}>
+            {model ? (
+              <TouchableOpacity
+                style={[
+                  styles.imageContainer,
+                  {
+                    height: imageHeight,
+                    width: imageWidth,
+                    borderWidth: source ? 0 : 2,
+                  },
+                ]}
+                onPress={this.onSelectImage.bind(this)}>
+                {source ? (
+                  <Image
+                    source={source}
+                    style={{
+                      height: imageHeight,
+                      width: imageWidth,
+                    }}
+                    resizeMode="contain"
+                  />
+                ) : (
+                  <Text style={styles.text}>Select Picture</Text>
+                )}
+              </TouchableOpacity>
+            ) : (
+              <View>
+                <Text
+                  style={{
+                    paddingLeft: 10,
+                    color: '#000',
+                    fontSize: 30,
+                    marginTop: 30,
+                  }}>
+                  Leaf Herb
+                </Text>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => this.props.navigation.navigate('Details')}>
+                  <Icon
+                    name="search"
+                    size={50}
+                    color="#fff"
+                    style={{marginVertical: 10}}
+                  />
+                  <Text style={styles.buttonText}>Search</Text>
+                </TouchableOpacity>
+                {renderButton(mobile)}
+              </View>
+            )}
+            <View>{this.renderResults()}</View>
+          </ScrollView>
         </View>
-        <View>
-            {this.renderResults()}
-        </View> */}
-      </ScrollView>
       </View>
-      </SafeAreaView>
-      
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
   imageContainer: {
     borderColor: blue,
     borderRadius: 5,
-    alignItems: "center"
+    alignItems: 'center',
+    marginTop: 10,
+    // marginLeft:30
   },
   text: {
-    color: blue
+    color: blue,
   },
   button: {
-    width: 200,
     backgroundColor: blue,
     borderRadius: 10,
-    height: 40,
+    height: 100,
+    width: 150,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 10
+    marginBottom: 10,
+    marginTop: 50,
+    // marginLeft: 100
   },
   buttonText: {
     color: 'white',
-    fontSize: 15
+    fontSize: 15,
   },
   box: {
     position: 'absolute',
@@ -213,5 +268,5 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     top: 0,
-  }
+  },
 });
